f36939581586822aff0e17af1bb991697b1106f5
Joseph Hnatek <24803352+JoeHnatek@users.noreply.github.com>
2023-01-18 20:10:40 -0600

    Cleaned variable names. Added support to display addresses and instructions of a rim file after displaying the checksum stats.

--- xor.py
+++ xor.py
@@ -11,18 +11,27 @@ import os
 PAD = b'\x80'   # Padding byte
 LOC = b'\x40'   # start Line Of Coding
 
-def getData(filepath: str) -> list:
+def get_data(filepath: str) -> list:
     """
     Opens filepath in read-binary mode
     Read 2 bytes at a time (12bit words)
     Skip LOC and PAD bytes.
     """
-    data = []
+    intruction_data = []
+    addr_data = []
     with open(filepath, mode="rb") as f:    # Open file in binary read mode
             while(word := f.read(2)):   # Read word by word
                 if(PAD not in word):    # Ignore padding
                     if LOC in word:
-                        continue
+                        addr = ''
+                        for e in word:
+                            # Convert hex to binary,
+                            # Strip python bin prefix 0b
+                            # Fill left most bits with 0 to ensure a length of 8
+                            # Strip meta-data bits 7 & 8
+                            addr += bin(e)[2:].zfill(8)[2:]
+                        addr_data.append(addr)
+                        
                     else:
                         binary_value = ''
                         for e in word:
@@ -31,9 +40,10 @@ def getData(filepath: str) -> list:
                             # Fill left most bits with 0 to ensure a length of 8
                             # Strip meta-data bits 7 & 8
                             binary_value += bin(e)[2:].zfill(8)[2:]
-                        data.append(binary_value)
+                        intruction_data.append(binary_value)
+                        
 
-    return data
+    return addr_data, intruction_data
 
 def xor(data: list) -> int:
     """
@@ -56,11 +66,37 @@ def bin_to_octal(bin_octals: list) -> str:
 
     return octals
 
+def convert_into_words(data: list) -> list:
+    """
+    Convert data into octal words
+    Useful in getting the addresses and instructions of a file
+    """
+    converted_data = []
+
+    for d in data:
+        converted_bits = [d[i:i+3] for i in range(0, len(d), 3)]    # Convert 12bits to 4 3bits
+        converted_octal = bin_to_octal(converted_bits)
+        converted_data.append(converted_octal)
+
+    return converted_data
+
+def print_addr_intruct(converted_addr: list, converted_instruct: list, addresses: list, instructions: list):
+    print(f"===== Addresses and Instructions =====")
+    print("{:<11} {:<10} {:<12} {}".format("Address", "Instruction", "bAddress", "bInsturction"))    # Magic number formating :)
+    for x in range(len(converted_addr)):    # For each address, output its respective data
+        print(f"{converted_addr[x]:<12}{converted_instruct[x]:<12}{addresses[x]:<12} {instructions[x]}")
+
+def printStats(filepath: str, decimal: int, binary: str, octal: str):
+    print(f"===== Checksum for {filepath} =====")
+    print(f"Decimal:\t{decimal}")
+    print(f"Binary:\t{binary}")
+    print(f"Octal:\t\t{octal}")
+
 def main(filepath: str) -> None:
 
-    data = getData(filepath)
+    addresses, instructions = get_data(filepath)
 
-    result = xor(data)
+    result = xor(instructions)
 
     binary_checksum = bin(result)[2:].zfill(12)  # Convert decimal to 12bits
     bin_octals = [binary_checksum[i:i+3] for i in range(0, len(binary_checksum), 3)]    # Convert 12bits to 4 3bits
@@ -69,11 +105,11 @@ def main(filepath: str) -> None:
 
     printStats(filepath, result, binary_checksum, octal)
 
-def printStats(filepath: str, decimal: int, binary: str, octal: str):
-    print(f"===== Checksum for {filepath} =====")
-    print(f"Decimal:\t{decimal}")
-    print(f"Binary:\t{binary}")
-    print(f"Octal:\t\t{octal}")
+    c_addresses = convert_into_words(addresses)
+    c_instruct = convert_into_words(instructions)
+
+    print_addr_intruct(c_addresses, c_instruct, addresses, instructions)
+
 
 if __name__ == '__main__':
     try:
